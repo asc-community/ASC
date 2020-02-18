629a4787f19d4d4e7d7adf1e966859b73ab775af
Sasha.potapov2002 <Sasha.potapov2002@yandex.ru>
2020-02-18 14:26:51 +0300

    working on articles

--- article/urls.py
+++ article/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import article_view
+from .views import article_view, article_list_view
 
 urlpatterns = [
-    path("articles/<int:article_id>/<str:article_slug>", article_view, name="article")
+    path("articles/", article_list_view, name="article_list"),
+    path("articles/<int:article_id>/<str:article_slug>", article_view, name="article"),
 ]
+
--- article/views.py
+++ article/views.py
@@ -3,6 +3,24 @@ from .models import Article
 from django.http import Http404
 
 
+class PostArticleView:
+    CUT_TAG = "<cut/>"
+
+    @staticmethod
+    def cut(text):
+        if PostArticleView.CUT_TAG in text:
+            return text[:text.find(PostArticleView.CUT_TAG)]
+        else:
+            # TODO
+            return ""
+
+    def __init__(self, article):
+        self.title = article.title
+        self.brief = PostArticleView.cut(article.content)
+        self.author_name = str(article.author_id)
+        self.link = "/articles/" + str(article.id) + "/" + str(article.slug)
+
+
 def article_view(request, article_id, article_slug):
     try:
         article = Article.objects.get(id=article_id, slug=article_slug)
@@ -15,3 +33,13 @@ def article_view(request, article_id, article_slug):
     if request.user.is_authenticated:
         context["edit_link"] = "/admin/article/article/" + str(article_id) + "/change"
     return render(request, "one_article_view_template.html", context)
+
+
+def article_list_view(request):
+    # TODO
+    posts = Article.objects.all()
+    post_views = [PostArticleView(post) for post in posts]
+    context = {
+        "articles": post_views
+    }
+    return render(request, "article_list_view_template.html", context)
